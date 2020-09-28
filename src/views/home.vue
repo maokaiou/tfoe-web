217d0dc4342edc775bfb4c633c876298742a5585
maokaiou <2827017101@qq.com>
2020-09-28 11:32:12 +0800

    最基本修改

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    你好 
+    <a>大家好</a> 
+    <ul>
+      <li>我们</li>
+    </ul>  
+  </div>
+</template>
+<script>
+export default {
+    name: "home"
+}
+</script>
